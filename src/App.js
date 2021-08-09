a6ba1f6a50a9e15d8c10bc8fb59cf938f60801eb
siobhan <sr@saibhreas.com>
2021-08-09 16:53:44 -0400

    moving forward

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <div className="container">
         <Users />
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,36 +1,30 @@
 //Use rce to genreate a class based component from react snippets
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  // constructor () {
-  //   super();
-  //   this.state = {
-  //     id: 'id',
-  //     "login": "mojombo",
-  //     "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-  //     "html_url": "https://github.com/mojombo"
-  //   }
-  // }
-  
-  
-  render() {
-    const {login, avatar_url, html_url}= this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
 
-    return (
-      <div className= "card text-center">
-        <img src={avatar_url}
-          alt=""  
-          className="round-img" 
-          style={{ width: '60px'}} 
-        />
-
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-        </div>
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    )
-  }
+    </div>
+  );
+};
+
+
+UserItem.propTypes = {
+  uer: PropTypes.object.isRequired
 }
 
-export default UserItem
+export default UserItem;
